9bd4635688cc86b2ea5db5300fd69be77b368300
Saddam <akhmedkhanov.sa@gmail.com>
2019-10-25 14:06:43 +0300

    ready to work

--- app.js
+++ app.js
@@ -19,23 +19,81 @@ server = app.listen(3000)
 //socket.io instantiation
 const io = require("socket.io").listen(server);
 
+var arrayInfoUsername = new Array();
 
 // listen on every connetction
 io.on('connection', (socket) => {
-    console.log('New user connected')
+    console.log('New user connected. ID:' + socket.id);
 
     // default username
     socket.username = 'Anonymous'
 
+
+    socket.join('allUser');
+
+    socket.join(socket.id);
+
+    arrayInfoUsername.push([socket.id, socket.username]);
+
+    // var arr = new Array();
+    // Object.keys(socket.nsp.adapter.rooms['allUser'].sockets).map(i => arr.push(i)); //берем все подключенные к комнате сокеты 
+    socket.emit('connectUserList', {
+        allUser: arrayInfoUsername
+    })
+
+    // var infoForAnyUser = new Array();
+    // socket.on('io', (data) => {
+    //     infoForAnyUser.push([data.info, socket.username]);
+    // })
+    // console.log(infoForAnyUser);
+    socket.on('disconnect', () => {
+        for(let i = 0; i < arrayInfoUsername.length; i++) {
+            if(socket.id == arrayInfoUsername[i][0]) {
+                arrayInfoUsername.splice(i,1);
+            }
+        }
+        socket.emit('connectUserList', {
+            allUser: arrayInfoUsername
+        })
+    })
+
     // listen on change_username
     socket.on('change_username', (data) => {
         socket.username = data.username;
+        for(let i = 0; i < arrayInfoUsername.length; i++) {
+            if(socket.id == arrayInfoUsername[i][0]) {
+                arrayInfoUsername[i][1] = data.username + '';
+                console.log(1);
+            }
+            console.log(arrayInfoUsername[i][1])
+        }
+        socket.emit('connectUserList', {
+            allUser: arrayInfoUsername
+        })
     })
+
+    socket.on('message_on', (data) => {
+        socket.to(data.roomID).emit('message_on', {
+            mes: data.message
+        })
+    })
+
 
     // Listen on new_message
     socket.on('new_message', (data) => {
+        var room;
         // broadcast the new message
-        io.sockets.emit('new_message', {
+        if(data.roomID != 'allUser') {
+            for(let i = 0; i < arrayInfoUsername.length; i++) {
+                if(data.roomID == arrayInfoUsername[i][0]) {
+                    room = arrayInfoUsername[i][1];
+                }
+            }
+        } else {
+            room = 'allUser';
+        }
+        socket.to(data.roomID).emit('new_message', {
+            roomID: room,
             message: data.message,
             username: socket.username
         });
@@ -45,7 +103,8 @@ io.on('connection', (socket) => {
     socket.on('typing', (data) => {
         if(data.count.length > 0){
             socket.broadcast.emit('typing', {
-                username: socket.username
+                username: socket.username,
+                length: data.count.length
             })
         }
     })
--- public/chat.js
+++ public/chat.js
@@ -2,25 +2,73 @@ $(function () {
     // make connetction
     var socket = io.connect('http://localhost:3000')
 
+    // console.log(id);
     // buttons and input
     var message = $("#message")
     var username = $('#username')
     var send_message = $('#send_message')
     var send_username = $('#send_username')
     var chatroom = $('#chatroom')
+    var userList = $('#userList')
+    var button = new Array();
+
+    console.log(socket);
+
+    socket.emit('io', {
+        info: socket.id
+    })
+
+    var room = 'allUser';
+
+    socket.on('connectUserList', (data) => {
+        userList.html('');
+        for(let i = 0; i < data.allUser.length; i++) {
+            // let name = '';
+            // // console.log(data.allUser[i])
+            // // console.log(data.sockets[0].username);
+            // for(let j = 0; j < data.sockets.length; j++) {
+            //     if(data.sockets[j].id == data.allUser[i]) {
+            //         name = data.sockets[j].username;
+            //     }
+            // }
+            if(data.allUser[i][0] != socket.id) {
+                userList.append(`<p id="nameUser">${data.allUser[i][1]}</p> <button class = "button" id="${data.allUser[i][0]}">Написать сообщение</button>`);
+            }
+            button.push($('#' + data.allUser[i][0]));
+        }
+    })
+
+    $('#userList').click(function(event)  {
+        let target = event.target;
+        if(target.id != 'div') {
+            
+        }
+
+        room = target.id + '';
+        // console.log(room);
+
+        // socket.emit('message_on', {
+        //     roomID: target.attr('id'),
+        // })
+    })
 
     // Emit message
     send_message.click(function () {
         socket.emit('new_message', {
+            roomID: room,
             message: message.val()
         })
     })
+
+    //Emit message on room
+
 
     // Listen on new_message
     socket.on('new_message', (data) => {
         console.log(data);
         let time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
-        chatroom.append(`<div class = 'mes'><span class = 'time'>${time}</span><p class = 'message'>${data.username}: ${data.message}</p></div>`)
+        console.log(time);
+        chatroom.append(`<div class = 'mes'><span class = 'time'>${time}</span><p class = 'chat'>Из чата c ${data.roomID}</p><p class = 'message'>${data.username}: ${data.message}</p></div>`)
     })
 
     // Emit a username
@@ -38,6 +86,6 @@ $(function () {
 
     // Listen on typing
     socket.on('typing', (data) => {
-        $(feedback).html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+        $(feedback).html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
     })
 });
